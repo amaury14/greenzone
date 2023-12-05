133d6735201a7ad2787949055f18c2ebae92bba1
Amaury Chong <achong@carrasco.uruit.com>
2023-12-05 14:27:11 -0300

    Added components for sing in up. Added shadcn lib, button and form. Added icons and images. Added router and navigation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+
+import './globals.css';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import { Home } from './_root/pages';
+import AuthLayout from './_auth/AuthLayout';
+import Rootlayout from './_root/RootLayout';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className="flex h-screen">
+         <Routes>
+            {/* public routes */}
+            <Route element={<AuthLayout />}>
+                <Route path="/sing-in" element={<SigninForm />} />
+                <Route path="/sing-up" element={<SignupForm />} />
+            </Route>
+
+            {/* private routes */}
+            <Route element={<Rootlayout />}>
+                <Route index element={<Home />}/>
+            </Route>            
+         </Routes>
+    </main>
   )
 }
 
