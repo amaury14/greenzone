6599587298c0c9c99d75221d4e0368846c3b5f8e
Amaury Chong <achong@carrasco.uruit.com>
2023-12-09 20:04:07 -0300

    fixed query

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -145,11 +145,16 @@ export const useGetPosts = () => {
     return useInfiniteQuery({
         queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
         queryFn: getInfinitePosts,
+        initialPageParam: 0,
         getNextPageParam: (lastPage) => {
-            if (lastPage && lastPage?.documents?.length === 0) return null;
+            // If there's no data, there are no more pages.
+            if (lastPage?.documents?.length === 0) {
+                return null;
+            }
 
-            const lastId = lastPage?.documents[lastPage?.documents?.length - 1]?.$id;
-
+            // Use the $id of the last document as the cursor.
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const lastId = (lastPage as any)?.documents[(lastPage as any)?.documents?.length - 1]?.$id;
             return lastId;
         }
     });
